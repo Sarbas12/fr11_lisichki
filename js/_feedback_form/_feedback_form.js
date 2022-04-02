9ba6f742621776cc45752e76ce978e60b829cff7
Neoalina <neo-alina@yandex.ru>
2022-04-02 15:10:39 +0300

    Making modal form

--- js/_feedback_form/_feedback_form.js
+++ js/_feedback_form/_feedback_form.js
@@ -1,8 +1,22 @@
-let feedback = document.getElementById('offer_button');
-feedback.onclick = () => {
-    document.querySelector(".feedbackForm").classList.add("feedbackForm_visible");
-};
+// let feedback = document.getElementById('offer_button');
+// feedback.onclick = () => {
+//     document.querySelector(".feedbackForm").classList.add("feedbackForm_visible");
+// };
 
+
+
+
+// feedback.onclick = show('block');
 document.getElementById('closeForm').onclick = () => {
-    document.querySelector(".feedbackForm").classList.toggle("feedbackForm_visible");
+    document.querySelector(".modalForm").style.display = 'none';
+    document.querySelector(".filter").style.display = 'none';
+};
+document.getElementById('offer_button').addEventListener('click', show);
+
+var show = function(state) {
+    document.getElementById('modalForm').style.display = state;
+    document.getElementById('filter').style.display = state;
+};
+offer_button.onclick = (state) => {document.getElementById('modalForm').style.display = state;
+document.getElementById('filter').style.display = state;
 };
